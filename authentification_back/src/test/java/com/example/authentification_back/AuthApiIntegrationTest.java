2a7c125fba8b2b787ce7d841cb2fefb557c29a82
mirija-712 <steveravalomanda@gmail.com>
2026-03-30 00:56:13 +0400

    tp2

--- authentification_back/src/test/java/com/example/authentification_back/AuthApiIntegrationTest.java
+++ authentification_back/src/test/java/com/example/authentification_back/AuthApiIntegrationTest.java
@@ -5,7 +5,7 @@ import com.example.authentification_back.service.AuthService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
